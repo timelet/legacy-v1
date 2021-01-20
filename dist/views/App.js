1d9913a278e339701c727c1e4699622cf0d50201
openscript <1105080+openscript@users.noreply.github.com>
2021-01-20 12:27:23 +0000

    Deploying to gh-pages from  @ 7086a745bc0e8caae7b03f22a51da510092d08f8 🚀

---
+++ dist/views/App.js
@@ -0,0 +1,6 @@
+import React from "../../_snowpack/pkg/react.js";
+export default function App({}) {
+  return /* @__PURE__ */ React.createElement("div", {
+    className: "App"
+  }, "Timelet");
+}
